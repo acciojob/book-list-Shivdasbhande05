ef9cce8774cd325341f903682a3be3a6ebbf7d30
Shivdasbhande05 <shivdasbhande@gmail.com>
2025-01-12 20:03:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 //your JS code here. If required.
+const submitBtn = document.querySelector("#submit");
+
+var bookList = document.getElementById("book-list");
+
+
+
+submitBtn.addEventListener("click" , newRow);
+
+function newRow() {
+    var title = document.getElementById("title").value.trim();
+    var author = document.getElementById("author").value.trim();
+    var isbn = document.getElementById("isbn").value.trim();
+
+    const row = document.createElement("tr");
+     row.innerHTML = `
+    <td>${title}<td>
+    <td>${author}<td>
+    <td>${isbn}<td>
+    <td><button type="submit" id="delete">Delete</button>`;
+
+    bookList.appendChild(row);
+    clear();
+
+    //remove data from table
+    var removeBtn = row.querySelector("#delete");
+
+    removeBtn.addEventListener("click" , () => {
+        bookList.removeChild(row);
+    })
+}
+
+function clear(){
+    document.getElementById("title").value = "";
+    document.getElementById("author").value = "";
+    document.getElementById("isbn").value = "";
+}
+
